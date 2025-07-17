93c553801176f17b6d5881df14f8f50dadc77e61
YATHARTH-77 <yatharthm.2006@gmail.com>
2025-07-17 12:36:27 +0530

    fixes

--- Preprocess/preprocessing.py
+++ Preprocess/preprocessing.py
@@ -55,7 +55,7 @@ def display_comparison(original, processed):
 #processed = preprocess_image(image_path)
 #im=cv.imread(image_path)
 #display_comparison(im,processed)
-process_folder('Preprocess\input','CRAFT Detection Model\\test_images')
+process_folder('input','../CRAFT Detection Model/test_images')
 #filename = os.path.basename(image_path)
 #output_path = os.path.join('preprocessed output', f"pre_{filename}")
 #cv.imwrite(output_path, processed)
