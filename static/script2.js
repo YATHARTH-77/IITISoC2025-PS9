04464ffb07e016efa1241ce10d62f4d42c4d1760
Prince Nagar <princenagar631@gmail.com>
2025-07-24 22:06:57 +0530

    Delete static directory

--- static/script2.js
+++
@@ -1,9 +0,0 @@
-console.log("Script loaded successfully!!!!!!!");
-let jsonData = fetch('/static/data.json')
-  .then(response => response.json())
-  .then(data => {
-    console.log("Data fetched successfully:", data);
-  })
-  .catch(error => {
-    console.error("Error fetching data:", error);
-  });
