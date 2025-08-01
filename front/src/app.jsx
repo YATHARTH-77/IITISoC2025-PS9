40d9a852b8e6ae24cddb1addd2831a0566b120df
Samay107 <samaymulchandani24@gmail.com>
2025-08-01 22:42:34 +0530

    changes app.jsx changes

--- front/src/app.jsx
+++ front/src/app.jsx
@@ -6,8 +6,7 @@ import { ResultsView } from './ResultView.jsx';
 // Main App Component
 function App() {
   const backendURL = 
-                      process.env.REACT_APP_API_URL || 
-                     'http://localhost:5000';
+                      process.env.REACT_APP_API_URL;
 
 
 
